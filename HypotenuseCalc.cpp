40ba007d5e653945e61f3655cd4783d58dd14230
Tawwing <weplayingroblox3@gmail.com>
2026-03-17 23:24:18 +0630

    Added HypotenuseCalc.cpp

--- HypotenuseCalc.cpp
+++ HypotenuseCalc.cpp
@@ -8,9 +8,7 @@ int main() {
         std::cin >> a;
         std::cout << "\nEnter Side B: " << std::endl;
         std::cin >> b;
-
         c = sqrt(pow(a, 2) + pow(b, 2));
-
         std::cout << "Side C: " << c << std::endl;
     }
 
