f50e73c19808f5acd5e9db045a5b7b012fec5ffa
Tawwing <weplayingroblox3@gmail.com>
2026-03-17 23:23:03 +0630

    Add hypotenuse calculation program
    Implement a program to calculate the hypotenuse using user input for sides A and B.

---
+++ HypotenuseCalc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    while (true) {
+        double a; double b; double c;
+        std::cout << "Enter Side A: " << std::endl;
+        std::cin >> a;
+        std::cout << "\nEnter Side B: " << std::endl;
+        std::cin >> b;
+
+        c = sqrt(pow(a, 2) + pow(b, 2));
+
+        std::cout << "Side C: " << c << std::endl;
+    }
+
+    return 0;
+}
