0895cd9572f819ffd6d82898b5528cb9db7929c8
Tawwing <weplayingroblox3@gmail.com>
2026-03-17 19:47:10 +0630

    Added Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,62 @@
+// Basic C++ Calculator 
+#include <iostream>
+#include <cmath>
+
+typedef std::string string;
+
+namespace Calculator {
+    using std::cout; 
+        static int value;
+    double Add(double First, double Second) {
+        return First + Second;
+    }
+    double Subtrack(double First, double Second) {
+        return First - Second;
+    }
+    double Mutiply(double First, double Second) {
+        return First * Second;
+    }
+    double Divide(double First, double Second) {
+        return First / Second;
+    }
+    double Power(double base, double exponent) {
+        return std::pow(base, exponent);
+    }
+    double Mod(double First, double Second) {
+        return fmod(First, Second);
+    }
+    void PrintAsnwer(int Result) {
+        cout << "The Asnwer is " << Result << std::endl;
+    }
+}
+
+int main() {
+    using namespace Calculator;
+    using std::cout; using  std::cin;
+    while (true) {
+        double FirstNumber = 0;
+        double SecondNumber = 0;
+        string Opeartor = "";
+        cout << "\nEnter First Number: ";
+        cin >> FirstNumber;
+        cout << "\nEnter Second Number: ";
+        cin >> SecondNumber;
+        cout << "\nEnter Operator: ";
+        cin >> Opeartor;
+        if (Opeartor == "+") {
+            PrintAsnwer(Add(FirstNumber, SecondNumber));
+        } else if (Opeartor == "-") {
+            PrintAsnwer(Subtrack(FirstNumber, SecondNumber));
+        } else if (Opeartor == "*") {
+            PrintAsnwer(Mutiply(FirstNumber, SecondNumber));
+        } else if (Opeartor == "/") {
+            PrintAsnwer(Divide(FirstNumber, SecondNumber));
+        } else if (Opeartor == "%") {
+            PrintAsnwer(Mod(FirstNumber, SecondNumber));
+        } else if (Opeartor == "^") {
+            PrintAsnwer(Power(FirstNumber, SecondNumber));
+        }
+        cout << "------------------------------------------";
+    }
+    return 0;
+}
